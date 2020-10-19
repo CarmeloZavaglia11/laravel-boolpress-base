6fb22fd4d2422f16b52c2126dac63bf8ecc27c35
Carmelo Zavaglia <carmeloz2220@gmail.com>
2020-10-19 15:30:48 +0200

    Complete

---
+++ database/seeds/AvatarsTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Avatar;
+use App\User;
+
+class AvatarsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker) {
+
+        $users = User::all();
+
+        foreach ($users as $user) {
+            $newAvatar = new Avatar();
+            $newAvatar->telefono = $faker->phoneNumber;
+            $newAvatar->avatar = $faker->imageUrl(400, 400);
+            $newAvatar->user_id = $user->id;
+            $newAvatar->save();
+        }
+    }
+}
